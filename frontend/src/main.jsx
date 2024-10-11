773eef4859a48cf8680f05609efd0db68a1b0aa3
ZiwenZhang <a60595677@gmail.com>
2024-10-10 23:21:34 -0400

    finished phase-2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -19,9 +19,9 @@ if (import.meta.env.MODE !== 'production') {
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
